957c2ea1a07d8a4daf7ee7580caec725a29da09c
etavington <timothy25172@gmail.com>
2024-09-24 22:20:35 +0800

    gRPC

---
+++ gRPC/Go/go_client_go_server_hello/client.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	"google.golang.org/grpc"
+
+	pb "example.com/helloworld"
+)
+
+const (
+	address = "localhost:50051"
+)
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewGreeterClient(conn)
+	name := "world"
+	r, err := c.SayHello(context.Background(), &pb.HelloRequest{Name: name})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.GetMessage())
+}
